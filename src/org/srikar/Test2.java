4d606996eb4856ce0781a3fc527ae8bba214961d
srikar-eranky <srikar.eranky@gmail.com>
2023-02-20 10:51:38 -0800

    initial commit

---
+++ src/org/srikar/Test2.java
@@ -0,0 +1,10 @@
+package org.srikar;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the second test project");
+	}
+
+}
